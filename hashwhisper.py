6455cb61ddab482a31e705d12d78ad970c811ea5
HackMaster <114884252+MalwareEZ@users.noreply.github.com>
2023-09-01 19:57:17 +0200

    Update hashwhisper.py

--- hashwhisper.py
+++ hashwhisper.py
@@ -5,7 +5,7 @@ import banner_md5
 from colorama import Fore
 from tqdm import tqdm
 
-# adding arguments
+# Function adding arguments
 def main():
     parser = argparse.ArgumentParser(description="tool to crack an md5 hash")
     parser.add_argument("-w", "--wordlist", dest="wordlist", help="wordlist path", required=True)
@@ -19,10 +19,12 @@ def main():
         os.system("cls" if os.name == "nt" else "clear")
 
         print(f"{Fore.GREEN}{banner_md5.banner_md5}")
-
+        
+        # Open the hash_path file
         with open(hash_path, "r") as hash_file:
             hash_lines = hash_file.readlines()
 
+        # Open the wordlist_path file
         with open(wordlist_path, "r", encoding="utf-8", errors="ignore") as wordlist_file:
             wordlist_lines = wordlist_file.readlines()
 
@@ -30,11 +32,12 @@ def main():
         
         wordlist_progress = tqdm(wordlist_lines, desc="wordlist progress", unit="word")
 
+        # progress bar
         for word_line in wordlist_progress:
             word_sep = word_line.strip()
             word_hashed = hashlib.md5(word_sep.encode("utf-8")).hexdigest()
 
-
+            # checks if word and hash are the same
             if any(word_hashed == hash_line.strip() for hash_line in hash_lines):
                 wordlist_progress.close()
                 print(f"\n{Fore.GREEN}[+]{Fore.WHITE} Password found :", word_sep)
